cf62131b8908be8606adce865c306c810eb69478
Prince Babu <princebabu4495@gmail.com>
2026-07-14 21:20:16 +0530

    Stop tracking .env

--- frontend/src/config/socketConfig.js
+++ frontend/src/config/socketConfig.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io(import.meta.env.VITE_SERVER_URL, {
+const socket = io(import.meta.env.VITE_SERVER_SOCKET_URL, {
   autoConnect: false,
   withCredentials: true,
 });
